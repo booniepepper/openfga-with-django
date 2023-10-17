c5ce2b33f3ddb4946cb4963abc0fdec8ec224c02
Justin "J.R." Hill <justin@so.dang.cool>
2023-10-16 18:10:40 -0700

    try it out

---
+++ openfga/views.py
@@ -0,0 +1,34 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+import asyncio
+import os
+import json
+import openfga_sdk
+from openfga_sdk.client import OpenFgaClient
+from openfga_sdk.credentials import Credentials, CredentialConfiguration
+
+configuration = openfga_sdk.ClientConfiguration(
+    api_host='api.us1.fga.dev',
+    store_id='01H8M76TB3P7EWC6T298WTJX2D',
+    credentials=Credentials(
+        method='client_credentials',
+        configuration=CredentialConfiguration(
+            api_issuer='fga.us.auth0.com',
+            api_audience='https://api.us1.fga.dev/',
+            client_id='P99BJ2XKlB1N8NdIPzN5Ew8mBegEz0FJ',
+            client_secret=os.getenv('OPENFGA_CLIENT_SECRET')
+        )
+    )
+)
+
+
+async def read_auth_models():
+    async with OpenFgaClient(configuration) as fga_client:
+        response = await fga_client.read_authorization_models()
+    return response
+
+
+def index(request):
+    response = asyncio.run(read_auth_models())
+    return HttpResponse(response)
