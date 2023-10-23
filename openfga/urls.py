3c952024eb69bb8840212c753f67601e776303a3
Justin "J.R." Hill <justin@so.dang.cool>
2023-10-23 13:29:02 -0700

    Add async and sync demos

--- openfga/urls.py
+++ openfga/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("auth_models", views.auth_models, name="auth_models"),
+    path("sync/auth_models", views.auth_models_sync, name="auth_models_sync"),
+    path("async/auth_models", views.auth_models, name="auth_models"),
 ]
--- openfga/views.py
+++ openfga/views.py
@@ -2,16 +2,20 @@
 from django.http import HttpResponse
 
 import os
+import openfga_sdk
+import openfga_sdk.credentials
 import openfga_sdk_sync
-from openfga_sdk_sync.client import OpenFgaClient
-from openfga_sdk_sync.credentials import Credentials, CredentialConfiguration
+import openfga_sdk_sync.credentials
 
-configuration = openfga_sdk_sync.ClientConfiguration(
+
+# Async (Existing)
+
+configuration = openfga_sdk.ClientConfiguration(
     api_host='api.us1.fga.dev',
     store_id='01H8M76TB3P7EWC6T298WTJX2D',
-    credentials=Credentials(
+    credentials=openfga_sdk.credentials.Credentials(
         method='client_credentials',
-        configuration=CredentialConfiguration(
+        configuration=openfga_sdk.credentials.CredentialConfiguration(
             api_issuer='fga.us.auth0.com',
             api_audience='https://api.us1.fga.dev/',
             client_id='P99BJ2XKlB1N8NdIPzN5Ew8mBegEz0FJ',
@@ -20,9 +24,33 @@ configuration = openfga_sdk_sync.ClientConfiguration(
     )
 )
 
-fga_client = OpenFgaClient(configuration)
+fga_client = openfga_sdk.OpenFgaClient(configuration)
 
 
-def auth_models(request):
-    models = fga_client.read_authorization_models()
+async def auth_models(request):
+    models = await fga_client.read_authorization_models()
+    return HttpResponse(models)
+
+
+# Synchronous (New, all imports subject to change)
+
+configuration_sync = openfga_sdk_sync.ClientConfiguration(
+    api_host='api.us1.fga.dev',
+    store_id='01H8M76TB3P7EWC6T298WTJX2D',
+    credentials=openfga_sdk_sync.credentials.Credentials(
+        method='client_credentials',
+        configuration=openfga_sdk_sync.credentials.CredentialConfiguration(
+            api_issuer='fga.us.auth0.com',
+            api_audience='https://api.us1.fga.dev/',
+            client_id='P99BJ2XKlB1N8NdIPzN5Ew8mBegEz0FJ',
+            client_secret=os.getenv('OPENFGA_CLIENT_SECRET')
+        )
+    )
+)
+
+fga_client_sync = openfga_sdk_sync.OpenFgaClient(configuration_sync)
+
+
+def auth_models_sync(request):
+    models = fga_client_sync.read_authorization_models()
     return HttpResponse(models)
