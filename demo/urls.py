7e267cf4cf91b26199c36ab73e2754a8b47b78fa
Justin "J.R." Hill <justin@so.dang.cool>
2023-10-23 15:05:31 -0700

    Rename the core Django stuff to 'demo'

--- demo/urls.py
+++ demo/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for openfga_with_django project.
+URL configuration for demo project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
